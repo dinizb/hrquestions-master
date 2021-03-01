d1f720c6ac3b8d8d28db291c41f23d55507d4c1a
dinizb <bpdiniz@gmail.com>
2021-03-01 19:07:38 -0300

    First version

---
+++ InterviewTestPagination/Controllers/TodoController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using InterviewTestPagination.Models;
+using InterviewTestPagination.Models.Todo;
+using System.Linq;
+
+namespace InterviewTestPagination.Controllers
+{
+    /// <summary>
+    /// 'Rest' controller for the <see cref="Todo"/>
+    /// model.
+    /// 
+    /// TODO: implement the pagination Action
+    /// </summary>
+    public class TodoController : ApiController
+    {
+
+        // TODO: [low priority] setup DI 
+        private readonly IModelService<Todo> _todoService = new TodoService();
+
+        [HttpGet]
+        public IEnumerable<Todo> Todos() {
+            return _todoService.List();
+        }
+
+        [HttpGet]
+        public IEnumerable<Todo> todoListPaginated(int size, int page, string orderBy = "creationDate", bool reOrderBy = true) {
+            page--;
+            switch (orderBy) {
+                case "id":
+                    if (reOrderBy) {
+                        return _todoService.List().OrderByDescending(item => item.Id).Skip(size * page).Take(size);
+                    }
+                    else {
+                        return _todoService.List().OrderBy(item => item.Id).Skip(size * page).Take(size);
+                    }
+                case "task":
+                    if (reOrderBy) {
+                        return _todoService.List().OrderByDescending(item => item.Task).Skip(size * page).Take(size);
+                    }
+                    else {
+                        return _todoService.List().OrderBy(item => item.Task).Skip(size * page).Take(size);
+                    }
+                case "creationDate":
+                    if (reOrderBy) {
+                        return _todoService.List().OrderByDescending(item => item.CreatedDate).Skip(size * page).Take(size);
+                    }
+                    else {
+                        return _todoService.List().OrderBy(item => item.CreatedDate).Skip(size * page).Take(size);
+                    }
+                default:
+                    return _todoService.List().OrderBy(item => item.CreatedDate).Skip(size * page).Take(size);
+            }
+        }
+    }
+}
---
+++ InterviewTestPagination/app/main.js
@@ -0,0 +1,168 @@
+﻿(function (angular) {
+    "use strict";
+
+    angular
+        .module("todoApp")
+        .directive("todoPaginatedList", [todoPaginatedList])
+        .directive("pagination", [pagination]);
+    /**
+     * Directive definition function of 'todoPaginatedList'.
+     * 
+     * TODO: correctly parametrize scope (inherited? isolated? which properties?)
+     * TODO: create appropriate functions (link? controller?) and scope bindings
+     * TODO: make appropriate general directive configuration (support transclusion? replace content? EAC?)
+     * 
+     * @returns {} directive definition object
+     */
+    function todoPaginatedList() {
+        var directive = {
+            restrict: "E", // example setup as an element only
+            templateUrl: "app/templates/todo.list.paginated.html",
+            scope: {}, // example empty isolate scope
+            controller: ["$scope", "$http", "$rootScope", controller],
+            link: link
+        };
+
+        function controller($scope, $http, $rootScope) { // example controller creating the scope bindings
+            $scope.todos = [];
+            // example of xhr call to the server's 'RESTful' api
+            let page = 1;
+            let size = 10;
+            let nItem = 0;
+            $scope.propertyName = "creationDate";
+            $scope.reOrderBy = true;
+            
+            $http.get("api/Todo/Todos")
+                .then(function (response) {
+                    nItem = response.data.length;
+                    $rootScope.$broadcast("loadData", { len: nItem, size: size });
+
+                    $http.get("api/Todo/todoListPaginated/", {
+                        params: {
+                            size: size
+                           ,page: page
+                           ,orderBy: $scope.propertyName
+                           ,reOrderBy: $scope.reOrderBy
+                        }
+                    }).then(function (response) {
+                        $scope.todos = response.data;
+                    });
+                });
+            $scope.sortBy = function (propertyName) {
+                $scope.reOrderBy = (propertyName !== null && $scope.propertyName === propertyName)
+                    ? !$scope.reOrderBy : false;
+                $scope.propertyName = propertyName;
+                $http.get("api/Todo/todoListPaginated/", {
+                    params: {
+                        size: size
+                       ,page: page
+                       ,orderBy: $scope.propertyName
+                       ,reOrderBy: $scope.reOrderBy
+                    }
+                }).then(
+                    function (response) {
+                        $scope.todos = response.data;
+                    }
+                );
+            };
+            $rootScope.$on("changeItemsPerPage", function (ev, args) {
+                if (args.val == "all") {
+                    $http.get("api/Todo/Todos")
+                        .then(function (response) {
+                            $scope.todos = response.data;
+                            $rootScope.$broadcast("loadData", { len: nItem, size: nItem });
+                        });
+                } else {
+                    size = args.val;
+                    $http.get("api/Todo/todoListPaginated/", {
+                        params: {
+                            size: size
+                           ,page: 1 //ToDo: RETURN TO FIRST PAGE TO AVOID SHOWING MSG LIKE PAGE 5 of 3
+                           ,orderBy: $scope.propertyName
+                           ,reOrderBy: $scope.reOrderBy
+                        }
+                    }).then(function (response) {
+                        $scope.todos = response.data;
+                        $rootScope.$broadcast("loadData", { len: nItem, size: size });
+                    });
+                }
+            });
+            $rootScope.$on("changePage", function (ev, args) {
+                page = args.val;
+                $http.get("api/Todo/todoListPaginated/", {
+                    params: {
+                        size: size
+                       ,page: page
+                       ,orderBy: $scope.propertyName
+                       ,reOrderBy: $scope.reOrderBy
+                    }
+                }).then(function (response) {
+                    $scope.todos = response.data;
+                });
+            }
+            );
+        }
+
+        function link(scope, element, attrs) { }
+        return directive;
+    }
+    /**
+     * Directive definition function of 'pagination' directive.
+     * 
+     * TODO: make it a reusable component (i.e. usable by any list of objects not just the Models.Todo model)
+     * TODO: correctly parametrize scope (inherited? isolated? which properties?)
+     * TODO: create appropriate functions (link? controller?) and scope bindings
+     * TODO: make appropriate general directive configuration (support transclusion? replace content? EAC?)
+     * 
+     * @returns {} directive definition object
+     */
+    function pagination() {
+        var directive = {
+            restrict: "E", // example setup as an element only
+            templateUrl: "app/templates/pagination.html",
+            scope: {}, // example empty isolate scope
+            controller: ["$scope", "$http", "$rootScope", controller],
+            link: link
+        };
+        function controller($scope, $http, $rootScope) {
+            $scope.paginationController = {}; 
+            $rootScope.$on("loadData", function (ev, args) {
+                $scope.paginationController.nItem = args.len;
+                $scope.paginationController.nPage = Math.ceil($scope.paginationController.nItem / args.size);
+            });
+            $scope.changeItemsPerPage = function () {
+                $rootScope.$broadcast("changeItemsPerPage", { val: $scope.paginationController.pageRow });
+            }
+            $scope.changePage = function () {
+                if ($scope.paginationController.page > 0 && $scope.paginationController.page <= $scope.paginationController.nPage) {
+                    $rootScope.$broadcast("changePage", { val: $scope.paginationController.page });
+                }
+            }
+            $scope.firstPage = function () {
+                $scope.paginationController.page = 1;
+                $rootScope.$broadcast("changePage", { val: 1 });
+            }
+            $scope.previousPage = function () {
+                if ($scope.paginationController.page > 1) {
+                    $scope.paginationController.page = $scope.paginationController.page - 1;
+                    $rootScope.$broadcast("changePage", { val: $scope.paginationController.page });
+                }
+            }
+            $scope.nextPage = function () {
+                if ($scope.paginationController.page < $scope.paginationController.nPage) {
+                    $scope.paginationController.page = $scope.paginationController.page + 1;
+                    $rootScope.$broadcast("changePage", { val: $scope.paginationController.page });
+                }
+            }
+            $scope.lastPage = function () {
+                $scope.paginationController.page = $scope.paginationController.nPage;
+                $rootScope.$broadcast("changePage", { val: $scope.paginationController.page });
+            }
+        }
+
+        function link(scope, element, attrs) { }
+        return directive;
+    }
+
+})(angular);
+
